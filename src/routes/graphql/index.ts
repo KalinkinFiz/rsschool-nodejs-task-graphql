af6944a1aca79a3f4aa817707ef5a87dd619f1cf
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-01-27 19:44:20 +0300

    feat: add query type

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,13 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import {
+  graphql,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+} from 'graphql';
+
 import { graphqlBodySchema } from './schema';
+import { user, post, memberType, profile } from './types';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify,
@@ -11,7 +19,44 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: graphqlBodySchema,
       },
     },
-    async function (request, reply) {},
+    async function (request, _reply) {
+      const schema: GraphQLSchema = new GraphQLSchema({
+        query: new GraphQLObjectType({
+          name: 'Query',
+          fields: {
+            users: {
+              type: new GraphQLList(user),
+              resolve() {
+                return fastify.db.users.findMany();
+              },
+            },
+
+            profiles: {
+              type: new GraphQLList(profile),
+              resolve() {
+                return fastify.db.profiles.findMany();
+              },
+            },
+
+            posts: {
+              type: new GraphQLList(post),
+              resolve() {
+                return fastify.db.posts.findMany();
+              },
+            },
+
+            memberTypes: {
+              type: new GraphQLList(memberType),
+              resolve() {
+                return fastify.db.memberTypes.findMany();
+              },
+            },
+          },
+        }),
+      });
+
+      return await graphql({ schema, source: request.body.query! });
+    },
   );
 };
 
