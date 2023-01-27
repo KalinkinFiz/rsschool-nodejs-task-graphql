788898951d6c7ac3f6e4c5f8814d61b21738cdf3
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-01-28 01:37:22 +0300

    feat: add getById 4 operations

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -4,6 +4,7 @@ import {
   GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
+  GraphQLString,
 } from 'graphql';
 
 import { graphqlBodySchema } from './schema';
@@ -30,25 +31,95 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
                 return fastify.db.users.findMany();
               },
             },
-
             profiles: {
               type: new GraphQLList(profile),
               resolve() {
                 return fastify.db.profiles.findMany();
               },
             },
-
             posts: {
               type: new GraphQLList(post),
               resolve() {
                 return fastify.db.posts.findMany();
               },
             },
-
             memberTypes: {
               type: new GraphQLList(memberType),
               resolve() {
                 return fastify.db.memberTypes.findMany();
+              },
+            },
+
+            user: {
+              type: user,
+              args: {
+                id: { type: GraphQLString },
+              },
+              async resolve(_, args) {
+                const user = await fastify.db.users.findOne({
+                  key: 'id',
+                  equals: args.id,
+                });
+
+                if (!user) {
+                  throw fastify.httpErrors.notFound('User not found');
+                }
+
+                return user;
+              },
+            },
+            profile: {
+              type: profile,
+              args: {
+                id: { type: GraphQLString },
+              },
+              async resolve(_, args) {
+                const profile = await fastify.db.profiles.findOne({
+                  key: 'id',
+                  equals: args.id,
+                });
+
+                if (!profile) {
+                  throw fastify.httpErrors.notFound('Profile not found');
+                }
+
+                return profile;
+              },
+            },
+            post: {
+              type: post,
+              args: {
+                id: { type: GraphQLString },
+              },
+              async resolve(_, args) {
+                const post = await fastify.db.posts.findOne({
+                  key: 'id',
+                  equals: args.id,
+                });
+
+                if (!post) {
+                  throw fastify.httpErrors.notFound('Post not found');
+                }
+
+                return post;
+              },
+            },
+            memberType: {
+              type: memberType,
+              args: {
+                id: { type: GraphQLString },
+              },
+              async resolve(_, args) {
+                const memberType = await fastify.db.memberTypes.findOne({
+                  key: 'id',
+                  equals: args.id,
+                });
+
+                if (!memberType) {
+                  throw fastify.httpErrors.notFound('MemberType not found');
+                }
+
+                return memberType;
               },
             },
           },
